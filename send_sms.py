9da9f31fc60e3874f32f758742f8aca99699e375
richard-G <ricardoglass@gmail.com>
2020-10-19 18:02:17 +0100

    get twilio config info from env vars

--- send_sms.py
+++ send_sms.py
@@ -1,8 +1,11 @@
 from twilio.rest import Client
+import os
 
 # twilio config
-account_sid = 'AC97ff662869325707dcbc249e2c3865ad'
-auth_token = '4720dbaac3bf65a0f8bcaf253dba579c'
+# account_sid = 'AC97ff662869325707dcbc249e2c3865ad'
+account_sid = os.environ['twilio-sid']
+auth_token = os.environ['twilio-token']
+# auth_token = '4720dbaac3bf65a0f8bcaf253dba579c'
 client = Client(account_sid, auth_token)
 
 
